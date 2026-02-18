2be367026535b0a8e1ba63a613aa1f535263b189
kessiamilena <kessia.milena124@yahoo.com>
2026-02-18 13:43:20 -0300

    :construction: Criação da ProdutoService e inicio da ProdutoController

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VHBurguer.Applications.Services;
+using VHBurguer.DTOs.ProdutoDto;
+
+namespace VHBurguer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly ProdutoService _service;
+
+        public ProdutoController(ProdutoService service)
+        {
+            _service = service;
+        }
+
+        // autenticação do usuário
+
+        [HttpGet]
+        public ActionResult<List<LerProdutoDto>> Listar()
+        {
+            List<LerProdutoDto> produtos = _service.Listar();
+
+            //return StatusCode(200, produtos);
+            return Ok(produtos);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<LerProdutoDto> ObterPorId(int id)
+        {
+            LerProdutoDto produto = _service.ObterPorId(id);
+
+            if(produto == null) { 
+                //return StatusCode(404);
+                return NotFound();
+            }
+
+            return Ok(produto);
+        }
+
+    }
+}
--- obj/Debug/net8.0/VHBurguer.AssemblyInfo.cs
+++ obj/Debug/net8.0/VHBurguer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VHBurguer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+449d36981bbbc5d032c89b91b1d682a2d149ff6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4caf3d2a539cdeb6b85197454155d77676c83b7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("VHBurguer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VHBurguer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
